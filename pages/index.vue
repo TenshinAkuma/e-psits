666eb3b9f87e86fcd7e92b4585c4caa00bfed4a5
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-07-30 09:18:22 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+   <div>
+       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <!-- Logo placeholder -->
+            <a class="navbar-brand" href="#">
+                <p>Philippine Society of Information Technology Students - Region XII</p>
+            </a>
+            <!-- Toggle button for mobile view -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Navbar links -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contacts</a>
+                    </li>
+                </ul>
+                <!-- Call to action buttons -->
+                <div class="d-flex">
+                    <a class="btn btn-outline-primary me-2" href="#">Login</a>
+                    <a class="btn btn-primary" href="#">Sign-Up</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+   </div>
+</template>
