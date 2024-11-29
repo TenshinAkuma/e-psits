db66fd9ea2979eb2e5180cf5a4d03802b544056b
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-11-30 03:34:36 +0800

    add create event modal UI

--- components/events/EventsCreateModal.vue
+++ components/events/EventsCreateModal.vue
@@ -5,7 +5,7 @@
 		data-bs-backdrop="static"
 		data-bs-keyboard="false"
 		tabindex="-1"
-		aria-labelledby="staticBackdropLabel"
+		aria-labelledby="createEventModal"
 		aria-hidden="true"
 		ref="createEventModalRef">
 		<div class="modal-dialog modal-dialog-centered">
@@ -24,8 +24,101 @@
 				</div>
 
 				<div class="modal-body">
-					<form id="createEvent">...</form>
+					<form id="createEvent">
+						<input
+							type="text"
+							placeholder="Event name"
+							class="form-control mb-3" />
+
+						<div class="d-flex gap-2 mb-2">
+							<div class="w-100">
+								<label
+									class="text-secondary"
+									for="eventDate"
+									>Date</label
+								>
+								<input
+									type="date"
+									class="form-control"
+									id="eventDate"
+									v-model="dateInput" />
+							</div>
+							<div>
+								<label
+									class="text-secondary"
+									for="eventTime"
+									>Time</label
+								>
+								<input
+									type="time"
+									class="form-control w-100"
+									id="eventTime"
+									v-model="timeInput" />
+							</div>
+						</div>
+						<p
+							style="font-size: 0.75rem"
+							class="fst-italic text-secondary">
+							This event will start on
+							{{ formatDate(dateInput) }} at
+							{{ timeInput }}
+						</p>
+
+						<select class="form-select mb-3" v-model="type">
+							<option hidden selected disabled value="">
+								Choose an event type
+							</option>
+							<option
+								v-for="(type, index) in eventTypes"
+								:key="index"
+								:value="type">
+								{{ type }}
+							</option>
+						</select>
+
+						<input
+							v-if="type == 'Virtual'"
+							type="text"
+							placeholder="Meeting link"
+							class="form-control mb-3" />
+
+						<div v-else>
+							<input
+								type="text"
+								placeholder="Venue"
+								class="form-control mb-3" />
+
+							<input
+								type="text"
+								placeholder="Address"
+								class="form-control mb-3" />
+						</div>
+
+						<select class="form-select mb-3">
+							<option value="" hidden selected>
+								Choose category
+							</option>
+							<option
+								v-for="(
+									category, index
+								) in eventCategories"
+								:key="index"
+								:value="category">
+								{{ category }}
+							</option>
+						</select>
+
+						<textarea
+							class="form-control mb-3"
+							placeholder="Description"
+							rows="5"
+							style="
+								max-height: 288px;
+								resize: vertical;
+							" />
+					</form>
 				</div>
+
 				<div class="modal-footer">
 					<button
 						data-bs-dismiss="modal"
@@ -49,6 +142,20 @@
 	const createEventModalRef = ref(null);
 	let createEventModal;
 
+	const date = ref(new Date());
+	const type = ref("");
+
+	const dateInput = computed(() => {
+		return new Date().toISOString().split("T")[0];
+	});
+
+	const timeInput = computed(() => {
+		const _date = new Date();
+		const hours = _date.getHours().toLocaleString().padStart(2, "0");
+		const minutes = _date.getMinutes().toString().padStart(2, "0");
+		return `${hours}:${minutes}`;
+	});
+
 	onMounted(() => {
 		// Access the global `bootstrap` object to initialize the Modal
 		if (createEventModalRef.value) {
@@ -63,4 +170,13 @@
 			createEventModal.hide();
 		}
 	};
+
+	const eventCategories = [
+		"Competition",
+		"Workshop",
+		"Career fair",
+		"Keynote speech",
+	];
+
+	const eventTypes = ["Face-to-face", "Virtual"];
 </script>
