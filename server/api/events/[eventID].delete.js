3db2f210d07a182b5f4499248331f200f07f2d8a
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-12-01 02:16:39 +0800

    add delete event api

---
+++ server/api/events/[eventID].delete.js
@@ -0,0 +1,9 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+export default defineEventHandler(async (event) => {
+	const client = await serverSupabaseClient(event);
+	const { eventID } = event.context.params;
+
+	return (await client.from("events").delete().eq("id", eventID).single())
+		.data;
+});
