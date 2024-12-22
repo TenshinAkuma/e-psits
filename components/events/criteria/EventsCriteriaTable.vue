f76cdb612c8bb621478035a4f43f1bb143d9ae82
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-12-23 02:45:31 +0800

    add event criteria table

--- components/events/criteria/EventsCriteriaTable.vue
+++ components/events/criteria/EventsCriteriaTable.vue
@@ -1,26 +1,49 @@
 <template>
 	<div class="table-responsive rounded-3">
-		<table class="table table-hover table-borderless">
+		<table class="table table-borderles align-middle">
 			<thead class="table-secondary">
 				<tr>
-					<th scope="col">#</th>
 					<th scope="col">Name</th>
 					<th scope="col">Description</th>
 					<th scope="col">Rating</th>
+					<th scope="col"></th>
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-					<th scope="row">1</th>
-					<td>Mark</td>
-					<td>Otto</td>
-					<td>@mdo</td>
+				<tr
+					v-for="criteria in criteriaData.criteria"
+					:key="criteria.id"
+					style="height: 72px">
+					<td class="h5 fw-bold">{{ criteria.name }}</td>
+					<td class="text-secondary">
+						{{ criteria.description }}
+					</td>
+					<td class="h5 fw-bold">{{ criteria.rating }}%</td>
+					<td>
+						<div class="hstack gap-2">
+							<button
+								class="btn btn-success hstack gap-2 ms-auto">
+								<i class="bi bi-pen-fill" />
+							</button>
+							<button class="btn btn-danger">
+								<i class="bi bi-trash-fill" />
+							</button>
+						</div>
+					</td>
 				</tr>
 			</tbody>
 		</table>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+	const eventID = useRoute().params.eventID;
+	const { data: criteriaData, status } = await useFetch(
+		`/api/events/${eventID}/criteria`,
+		{
+			method: "GET",
+		}
+	);
+</script>
 
 <style></style>
