c7de81f08531d88b8f2146396ffb38c6aaab37cd
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-12-23 02:09:09 +0800

    Add event criteria UI

---
+++ components/events/criteria/EventsCriteriaList.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="border rounded-3">
+		<div
+			class="d-flex justify-content-between align-items-center border-bottom px-3">
+			<h5 class="fw-bold m-0">Name</h5>
+			<div class="hstack gap-2">
+				<button class="btn btn-outline-success hstack gap-2">
+					<i class="bi bi-pen-fill" /><span>Edit</span>
+				</button>
+				<button class="btn btn-outline-danger">
+					<i class="bi bi-trash-fill" />
+				</button>
+			</div>
+		</div>
+		<div class="p-3">Description</div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style></style>
---
+++ components/events/criteria/EventsCriteriaTable.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="table-responsive rounded-3">
+		<table class="table table-hover table-borderless">
+			<thead class="table-secondary">
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">Name</th>
+					<th scope="col">Description</th>
+					<th scope="col">Rating</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<script setup></script>
+
+<style></style>
