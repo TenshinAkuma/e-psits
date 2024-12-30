831402141f320be1b8bd24a34e7400ab90228a47
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-12-31 01:23:18 +0800

    Create index.delete.js

---
+++ server/api/event-rules/[ruleId]/index.delete.js
@@ -0,0 +1,32 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+export default defineEventHandler(async (event) => {
+	const client = serverSupabaseClient(event);
+	const { ruleId } = event.context.params;
+	const body = await readBody(event);
+
+	try {
+		const { data: rulesData, error: rulesError } = (await client)
+			.from("event_rules")
+			.delete()
+			.eq("id", ruleId);
+
+		if (rulesError) {
+			throw new Error(rulesError.message);
+		}
+
+		return {
+			success: true,
+		};
+	} catch (err) {
+		console.error(
+			"Error occurred while deleting event rule",
+			err.message
+		);
+
+		return {
+			success: false,
+			error: err.message,
+		};
+	}
+});
