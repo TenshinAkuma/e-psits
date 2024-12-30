1df606a057aeb60398350762aef68394216662d3
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-12-30 21:19:18 +0800

    Create create.post.js

---
+++ server/api/event-rules/create.post.js
@@ -0,0 +1,29 @@
+import { serverSupabaseClient } from "#supabase/server";
+
+export default defineEventHandler(async (event) => {
+	const client = await serverSupabaseClient(event);
+
+	try {
+		const { data: rulesData, error: rulesError } = await client
+			.from("event_rules")
+			.insert(body)
+			.select("id, name, description")
+			.single();
+
+		if (rulesError) {
+			throw new Error(rulesError.message);
+		}
+
+		return {
+			success: true,
+			data: rulesData,
+		};
+	} catch (err) {
+		console.err("Error occurred while creating event rule", err.message);
+
+		return {
+			success: false,
+			error: err.message,
+		};
+	}
+});
