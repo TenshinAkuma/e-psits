f5201585770d3427283c81022775a4b8b5f8f4a3
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-12-31 01:45:56 +0800

    Create EventsTabsRules.vue

---
+++ components/events/tabs/EventsTabsRules.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<ul class="list-group">
+			<li class="list-group-item p-3">
+				<div class="d-flex justify-content-between">
+					<p class="fw-bold m-0">Name</p>
+					<div class="hstack gap-2">
+						<button class="btn btn-sm">
+							<i class="bi bi-pencil-fill" />
+						</button>
+						<button class="btn btn-sm">
+							<i class="bi bi-trash-fill" />
+						</button>
+					</div>
+				</div>
+				<p class="fs-7 text-secondary lh-sm">Description</p>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script setup></script>
+
+<style></style>
