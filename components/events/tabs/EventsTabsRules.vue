4b7630ff1b15e44f99ce537ccf6c8695ad8db706
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-12-31 03:57:05 +0800

    Update EventsTabsRules.vue

--- components/events/tabs/EventsTabsRules.vue
+++ components/events/tabs/EventsTabsRules.vue
@@ -4,9 +4,12 @@
 			<RulesCreate :rulesId="0" />
 		</div>
 		<ul class="list-group">
-			<li class="list-group-item p-3">
+			<li
+				v-for="rule in eventRules"
+				:key="rule.id"
+				class="list-group-item p-3">
 				<div class="d-flex justify-content-between">
-					<p class="fw-bold m-0">Name</p>
+					<p class="fw-bold m-0">{{ rule.name }}</p>
 					<div class="hstack gap-2">
 						<button class="btn btn-sm">
 							<i class="bi bi-pencil-fill" />
@@ -16,12 +19,27 @@
 						</button>
 					</div>
 				</div>
-				<p class="fs-7 text-secondary lh-sm">Description</p>
+				<p
+					class="fs-7 text-secondary lh-sm"
+					style="max-width: 56ch">
+					{{ rule.description }}
+				</p>
 			</li>
 		</ul>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+	const eventRules = useEventRules();
+
+	const { data: _rulesData, status: _rulesStatus } = await useFetch(
+		"/api/event-rules",
+		{
+			method: "GET",
+		}
+	);
+
+	eventRules.value = _rulesData.value?.data;
+</script>
 
 <style></style>
