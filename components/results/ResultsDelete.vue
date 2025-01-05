b415bc07d74de9036dd8e4227f427c9efd65a2bf
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2025-01-05 21:18:14 +0800

    Update ResultsDelete.vue

--- components/results/ResultsDelete.vue
+++ components/results/ResultsDelete.vue
@@ -1,11 +1,31 @@
 <template>
-	<Dialog dialogId="deleteResult"></Dialog>
+	<Dialog :dialogId="`deleteResult-${registrationId}`" dialogTitle="Remove Participant Evaluation" openButtonStyle="btn-sm text-secondary">
+      <template #ButtonLabel>
+         <i class="bi bi-trash-fill" style="font-size: .5rem;"/>
+      </template>
+
+      <template #Body>
+         <b>This action will permanently delete the scores.</b> <br />
+				Are you sure you want to delete this?
+            {{ scores }}
+      </template>
+
+      <template #Submit>
+         <button class="btn btn-danger">Remove evaluation</button>
+      </template>
+   </Dialog>
 </template>
 
 <script setup>
    const props = defineProps({
-      resultId: [],
+      registrationId: Number,
    });
+
+   const eventScores = useEventScores()
+
+   const scores = computed(() => {
+      return eventScores.value?.filter((score) => score.registration_id == props.registrationId)
+   })
 </script>
 
 <style></style>
