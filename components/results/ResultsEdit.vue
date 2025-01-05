05359b45bd255728105ff1bf2b8876e378eea90f
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2025-01-06 03:24:12 +0800

    Update ResultsEdit.vue

--- components/results/ResultsEdit.vue
+++ components/results/ResultsEdit.vue
@@ -1,64 +1,74 @@
 <template>
-	<Dialog :dialogId="`editResult-${registrationId}`" dialogTitle="Remove Participant Evaluation" openButtonStyle="btn-sm text-secondary">
-      <template #ButtonLabel>
-         <i class="bi bi-trash-fill" style="font-size: .5rem;"/>
-      </template>
+	<Dialog
+		:dialogId="`editResult-${score.score_id}`"
+		dialogTitle="Edit Participant Evaluation"
+		openButtonStyle="btn-sm text-secondary">
+		<template #ButtonLabel>
+			<i class="bi bi-pencil-fill" style="font-size: 0.5rem" />
+		</template>
 
-      <template #Body>
-         <p>{{ participant }}</p>
-         <form>
-            <div
-					v-for="criterion in eventCriteria"
-					:key="criterion.id"
-					class="mb-3">
-					<label class="mb-1 fs-7">{{ criterion.name }}</label>
-					<div class="hstack gap-2">
-						<div class="input-group">
-							<span
-								class="input-group-text border-secondary bg-secondary bg-opacity-10"
-								>{{ `${criterion.rating}%` }}</span
-							>
-							<input
-								type="number"
-								class="form-control border-secondary"
-								placeholder="Score"
-								v-model="evaluationEdit.scores[criterion.id]"
-								min="0"
-								max="100"
-								required />
-						</div>
+		<template #Body>
+			<p class="text-start text-dark lh-sm fs-6">
+				Update <b>{{ `${getParticipantName}'s` }}</b> score on
+				<b>{{ `${eventScore.event_criteria.name}.` }}</b>
+			</p>
+			<form :id="`editResultForm-${score.score_id}`">
+				<div class="hstack gap-2">
+					<div class="input-group">
+						<span
+							class="input-group-text border-secondary bg-secondary bg-opacity-10">
+							{{ `${eventScore.event_criteria.rating}%` }}
+						</span>
+						<input
+							type="number"
+							class="form-control border-secondary"
+							placeholder="Score"
+							v-model="evaluationEdit.score"
+							min="0"
+							max="100"
+							required />
 					</div>
 				</div>
-         </form>
-         {{ scores }}
-      </template>
-   </Dialog>
+			</form>
+		</template>
+
+		<template #Submit>
+			<button
+				type="submit"
+				:form="`editResultForm-${score.score_id}`"
+				class="btn btn-success px-5">
+				Save
+			</button>
+		</template>
+	</Dialog>
 </template>
 
 <script setup>
    const props = defineProps({
-      registrationId: Number,
+      score: Object,
    });
 
    const evaluationEdit = ref({
-      scores: {},
-      registration_id: props.registrationId
-   })
+      score: props.score.score,
+      score_id: props.score.score_id,
+   });
 
    const eventScores = useEventScores();
-   const participantsRegistrations = useParticipantRegistrations()
-   const eventCriteria = useEventCriteria()
+   const participantsRegistrations = useParticipantRegistrations();
 
-   const participant = computed(() => {
-      return participantsRegistrations.value?.find(
-         (participant) =>
-            participant.id == props.registrationId
+   const eventScore = computed(() => {
+      return eventScores.value?.find((e) => e.id == props.score?.score_id);
+   });
+
+   const getParticipantName = computed(() => {
+      const registration_id = eventScore.value?.registration_id;
+
+      const participant = participantsRegistrations.value?.find(
+         (p) => p.id == registration_id
       );
-   })
 
-   const scores = computed(() => {
-      return eventScores.value?.filter((score) => score.registration_id == props.registrationId)
-   })
+      return `${participant.participants.first_name} ${participant.participants.last_name}`;
+   });
 </script>
 
 <style></style>
