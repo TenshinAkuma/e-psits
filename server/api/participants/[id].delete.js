b2c61c0f3bca15a5747ed082105d8bc0a458f5d3
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2025-01-11 06:26:17 +0800

    Update [id].delete.js

--- server/api/participants/[id].delete.js
+++ server/api/participants/[id].delete.js
@@ -2,13 +2,30 @@ import { serverSupabaseClient } from "#supabase/server";
 
 export default defineEventHandler(async (event) => {
 	const client = await serverSupabaseClient(event);
-	const { participantID } = event.context.params;
+	const { id } = event.context.params;
 
-	return (
-		await client
+	try {
+		const { error: participantError } = await client
 			.from("participants")
 			.delete()
-			.eq("id", participantID)
-			.single()
-	).data;
+			.eq("id", id);
+
+		if (participantError) {
+			throw new Error(participantError.message);
+		}
+
+		return {
+			success: true,
+		};
+	} catch (error) {
+		console.error(
+			"Error occurred while deleting participant",
+			error.message
+		);
+
+		return {
+			success: false,
+			error: error.message,
+		};
+	}
 });
