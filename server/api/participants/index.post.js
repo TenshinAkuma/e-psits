76fa3255a0092e40356aa8e2485cc85912bef7ab
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2025-01-11 06:13:38 +0800

    Update index.post.js

--- server/api/participants/index.post.js
+++ server/api/participants/index.post.js
@@ -4,7 +4,31 @@ export default defineEventHandler(async (event) => {
 	const client = await serverSupabaseClient(event);
 	const body = await readBody(event);
 
-	console.log(body);
-	return (await client.from("participants").insert(body).select().single())
-		.data;
+	try {
+		const { data: participantData, error: participantError } =
+			await client
+				.from("participants")
+				.insert(body)
+				.select("*")
+				.single();
+
+		if (participantError) {
+			throw new Error(participantError.message);
+		}
+
+		return {
+			success: true,
+			data: participantError,
+		};
+	} catch (error) {
+		console.error(
+			"Error occurred while loading participant details",
+			error.message
+		);
+
+		return {
+			success: false,
+			error: error.message,
+		};
+	}
 });
