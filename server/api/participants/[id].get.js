503a497b7c0c428fe735164af3efe092399fa3f4
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2025-01-11 06:26:20 +0800

    Update [id].get.js

--- server/api/participants/[id].get.js
+++ server/api/participants/[id].get.js
@@ -2,13 +2,33 @@ import { serverSupabaseClient } from "#supabase/server";
 
 export default defineEventHandler(async (event) => {
 	const client = await serverSupabaseClient(event);
-	const { participantID } = event.context.params;
+	const { id } = event.context.params;
 
-	return (
-		await client
-			.from("participants")
-			.select("*")
-			.eq("id", participantID)
-			.single()
-	).data;
+	try {
+		const { data: participantData, error: participantError } =
+			await client
+				.from("participants")
+				.select("*")
+				.eq("id", id)
+				.single();
+
+		if (participantError) {
+			throw new Error(participantError.message);
+		}
+
+		return {
+			success: true,
+			data: participantData,
+		};
+	} catch (error) {
+		console.error(
+			"Error occurred while loading participant",
+			error.message
+		);
+
+		return {
+			success: false,
+			error: error.message,
+		};
+	}
 });
