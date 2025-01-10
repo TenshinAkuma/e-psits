ff1ea744344d9243eaff3b47ba1bebf8afcf89bc
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2025-01-11 06:02:03 +0800

    init refactor api endpoints

--- server/api/participants/index.get.js
+++ server/api/participants/index.get.js
@@ -3,11 +3,27 @@ import { serverSupabaseClient } from "#supabase/server";
 export default defineEventHandler(async (event) => {
 	const client = await serverSupabaseClient(event);
 
-	return (
-		await client
-			.from("participants")
-			.select(
-				"*, participant_registrations!left (events(id, title), registration_status), institutions (id, name)"
-			)
-	).data;
+	try {
+		const { data: participantsData, error: participantsError } =
+			await client.from("participants").select("*");
+
+		if (participantsError) {
+			throw new Error(participantsError.message);
+		}
+
+		return {
+			success: true,
+			data: participantsData,
+		};
+	} catch (error) {
+		console.error(
+			"Error occurred while loading participants",
+			error.message
+		);
+
+		return {
+			success: false,
+			error: error.message,
+		};
+	}
 });
