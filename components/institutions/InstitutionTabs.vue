b20e32b8b50f6264e0f08a865b646e101583c940
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2025-02-02 00:51:45 +0800

    Create InstitutionTabs.vue

---
+++ components/institutions/InstitutionTabs.vue
@@ -0,0 +1,37 @@
+<template>
+	<ul class="nav nav-tabs">
+		<li class="nav-item">
+			<NuxtLink
+				class="nav-link"
+				:class="{ active: isActive('details') }"
+				:to="`/admin/institutions/${institutionId}`"
+				>Details</NuxtLink
+			>
+		</li>
+	</ul>
+</template>
+
+<script setup>
+	const props = defineProps({
+		activeTab: {
+			type: String,
+			required: true,
+		},
+   });
+
+   const institutionId = useRoute().params.institutionId;
+   
+	const isActive = (tab) => {
+		return tab === props.activeTab;
+	};
+</script>
+
+<style scoped>
+	.nav-link {
+		color: #424242;
+	}
+	.nav-link.active {
+		color: #242424 !important;
+		font-weight: bold;
+	}
+</style>
